07a4726c415bb17c7c8707f55595b7af49ade5eb
Brian Maranville <brian.maranville@nist.gov>
2012-10-05 12:41:46 -0400

    android troubleshooting

--- server.py
+++ server.py
@@ -557,7 +557,7 @@ def serve(debug=False, sio_port=8001):
         #cookie_secret = cookie.get_cookie(),
         #xsrf_cookies = True,
         #login_url = "/login",
-        flash_policy_port = 843,
+        flash_policy_port = 10843,
         flash_policy_file = os.path.join(ROOT, 'flashpolicy.xml'),
         socket_io_port = sio_port,
         debug = debug,
