b4d6a3037bbceb510787d604dee01eb0b3ef22ec
Brian Ben Maranville <brian.maranville@nist.gov>
2017-04-05 19:07:31 -0400

    why was there a nonsensical zero at the end of the addClass?

--- nicejs/build_slices.sh
+++ nicejs/build_slices.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #export LD_LIBRARY_PATH=$GLACIER_HOME/lib
-export NICE_HOME=/home/bbm/pydev/NICE
+export NICE_HOME=/home/bbm/dev/NICE
 export PATH=/opt/Ice-3.6.1/bin:$PATH
 export ICE_HOME=/opt/Ice-3.6.1
 slice2js --underscore --output-dir=generated -I$ICE_HOME/slice/ -I$NICE_HOME/slice/ $NICE_HOME/slice/*.ice
