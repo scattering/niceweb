8cf5f975e3f80e90e70c0d9c3410a35a066a603e
bbm <brian.maranville@nist.gov>
2018-01-02 10:55:38 -0500

    adding service worker cache... loads from cache first, then updates cache each time if possible

--- nicejs/build_slices.sh
+++ nicejs/build_slices.sh
@@ -2,6 +2,6 @@
 
 #export LD_LIBRARY_PATH=$GLACIER_HOME/lib
 export NICE_HOME=/home/bbm/dev/NICE
-export PATH=/opt/Ice-3.6.1/bin:$PATH
-export ICE_HOME=/opt/Ice-3.6.1
+#export PATH=/opt/Ice-3.6.1/bin:$PATH
+#export ICE_HOME=/opt/Ice-3.6.1
 slice2js --underscore --output-dir=generated -I$ICE_HOME/slice/ -I$NICE_HOME/slice/ $NICE_HOME/slice/*.ice
