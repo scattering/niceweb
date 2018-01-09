b257717505a3ecffec1532e33112af204d4f0dd0
bbm <brian.maranville@nist.gov>
2018-01-09 12:24:09 -0500

    missing sw.js added!

---
+++ nicejs/sw.js
@@ -0,0 +1,29 @@
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.open('niceclient').then(function(cache) {
+      return cache.match(event.request).then(function(response) {
+        var fetchPromise = fetch(event.request).then(function(networkResponse) {
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        })
+        return response || fetchPromise;
+      })
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          // Return true if you want to remove this cache,
+          // but remember that caches are shared across
+          // the whole origin
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
